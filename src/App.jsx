420a8328ab036d87aacad9224ba7709c1affecbb
Salim104 <salimshaban885@gmail.com>
2024-09-07 17:44:39 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! ok 
-    </h1>
-  )
-}
+const App = () => (
+ <main className="relative">
+  Nav
+  <section className="xl:padding-1 wide:padding-r padding-b ">
+    hero
+  </section>
+  <section className="padding">
+   PopularProducts
+  </section>
+  <section className="padding">
+   SupperQuality
+  </section>
+  <section className="padding-x py-10">
+   Services
+  </section>
+  <section className="padding">
+   specialoffer
+  </section>
+
+ </main>
+)
+
+export default App;
